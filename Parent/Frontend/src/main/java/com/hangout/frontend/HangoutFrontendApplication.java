ded0aa0777a79066b79ffcbbff0f5f4ef21eef4e
charatkakkar <charatkakkar@gmail.com>
2022-08-14 17:55:26 -0400

    Added appconfig class

---
+++ Parent/Frontend/src/main/java/com/hangout/frontend/HangoutFrontendApplication.java
@@ -0,0 +1,9 @@
+package com.hangout.frontend;
+
+import org.springframework.boot.SpringApplication;
+
+public class HangoutFrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HangoutFrontendApplication.class, args);
+    }
+}
